9ba22e76631dce23693388f13733b398d3151998
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-02-17 19:04:58 -0500

    commit tree b7cabfea74032dfa4bf699d162573e196732fafb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array){
+    let resultObject = array.find(record => record.result === "W") 
+        if(resultObject){
+            return resultObject.year
+        }
+}
+
+//console.log(record.find(superbowlWin).year);
